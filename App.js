2e0616df736e5185b642e557504b70843ceecda1
강경필 <rudvlf0413@naver.com>
2018-01-26 20:14:45 +0900

    first upload - react-native, redux study

---
+++ App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import Timer from './components/Timer'
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <Timer />
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
---
+++ components/Button/index.js
@@ -0,0 +1,21 @@
+import React, { Component } from "react";
+import PropTypes from 'prop-types';
+import { TouchableOpacity } from 'react-native';
+import { FontAwesome } from '@expo/vector-icons';
+
+
+function Button({iconName, onPress}) {
+    return (
+        <TouchableOpacity onPressOut={onPress} >
+            <FontAwesome name={iconName} size={80} color="white" />
+        </TouchableOpacity>
+    )
+}
+
+Button.propTypes = {
+    iconName: PropTypes.string.isRequired,
+    onPress: PropTypes.func.isRequired
+}
+
+
+export default Button;
