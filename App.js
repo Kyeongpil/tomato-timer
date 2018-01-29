8edf5cf78bfd22d1a1204b32060a4e8e573eb877
강경필 <rudvlf0413@naver.com>
2018-01-30 01:16:16 +0900

    update

--- App.js
+++ App.js
@@ -1,20 +1,18 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import Timer from './components/Timer'
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import Timer from "./components/Timer";
+import reducer from "./reducer";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+
+let store = createStore(reducer);
 
 export default class App extends React.Component {
   render() {
     return (
-      <Timer />
+      <Provider store={store}>
+        <Timer />
+      </Provider>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ reducer.js
@@ -0,0 +1,95 @@
+// Import
+
+// Action
+
+const START_TIMER = "START_TIMER";
+const RESTART_TIMER = "RESTART_TIMER";
+const ADD_SECOND = "ADD_SECOND";
+
+// Action Creators
+
+function startTimer() {
+  return {
+    type: START_TIMER
+  };
+}
+
+function restartTimer() {
+  return {
+    type: RESTART_TIMER
+  };
+}
+
+function addSecond() {
+  return {
+    type: ADD_SECOND
+  };
+}
+
+// Reducer
+
+const TIMER_DURATION = 1500;
+const initialState = {
+  isCounting: false,
+  elapsedTime: 0,
+  timerDuration: TIMER_DURATION
+};
+
+function reducer(state = initialState, action) {
+  switch (action.type) {
+    case START_TIMER:
+      return applyStartTimer(state);
+    case RESTART_TIMER:
+      return applyRestartTimer(state);
+    case ADD_SECOND:
+      return applyAddSecond(state);
+    default:
+      return state;
+  }
+}
+
+// Reducer functions
+
+function applyStartTimer(state) {
+  return {
+    ...state,
+    isPlaying: true,
+    elapsedTime: 0
+  };
+}
+
+function applyRestartTimer(state) {
+  return {
+    ...state,
+    isPlaying: false,
+    elapsedTime: 0
+  };
+}
+
+function applyAddSecond(state) {
+  if (state.elapsedTime < TIMER_DURATION) {
+    return {
+      ...state,
+      elapsedTime: state.elapsedTime + 1
+    };
+  } else {
+    return {
+      ...state,
+      isPlaying: false
+    };
+  }
+}
+
+// export Action Creators
+
+const actionCreators = {
+  startTimer,
+  restartTimer,
+  addSecond
+};
+
+export { actionCreators };
+
+// export Reducer
+
+export default reducer;
